8acbe6f617a4709946d263a09928aed6b703db34
Gui Carminati <guicarminati@outlook.com>
2025-05-22 23:58:54 +0100

    Refactor code and finish the base version of the game

--- main.js
+++ main.js
@@ -1,37 +1,76 @@
 const prompt = require('prompt-sync')({sigint: true});
 
-const hat = '^';
-const hole = 'O';
-const fieldCharacter = '░';
-const pathCharacter = '*';
+const feature = {
+    hat: '^',
+    hole: 'O',
+    fieldCharacter: '░',
+    pathCharacter: '*'
+}
+const direction = {
+    up: 'u',
+    down: 'd',
+    left: 'l',
+    right: 'r'
+}
+const validInputs = Object.values(direction);  // ['u', 'd', 'l', 'r']
 
 class Field {
   constructor(field2dMatrix){
     this._field = field2dMatrix;
     this._x = 0;
     this._y = 0;
-    this._field[this._x][this._y] = '*';
+    this._field[0][0] = feature.pathCharacter; // set starting position
   }
+
+  runGame() {
+    let keepPlaying = true;
+    while(keepPlaying){
+        this.print();
+        keepPlaying = this.move(this.getDirection());
+    }
+  }
+
+  move(dir){
+    try{
+        this.updatePath(dir);
+        return true;
+    } catch(e){
+        console.error(e.message);
+        return false;
+    }
+}
+
+getDirection(){
+    while(true){    
+        let input = prompt('Which way? ').toLowerCase();
+
+        if(validInputs.includes(input)) return input; //
+
+        // continue while input is invalid
+        const error = new Error("Invalid input. Please one of the folowing characters: U (up), D (down), L (left) or R (right)");
+        console.error(error.message); 
+    }
+}
   
-  static generateField(numRows, numCols, holesPercentage){
+  static generateField(numRows=5, numCols=10, holesPercentage=0.2){
     holesPercentage = holesPercentage>1 ? holesPercentage/100 : holesPercentage; // adjust percentage to 0-1, if not yet
     if(holesPercentage>1 || holesPercentage<0 || numCols<=0 || numRows<=0) {
         throw new Error('invalid percentage, number of rows or columns ')
     }    
     const totalCells = numCols*numRows;
     const totalHoles = Math.floor(totalCells * holesPercentage);
-    let array2D = new Array(numRows).fill().map(() => new Array(numCols).fill('░'));
-    array2D[0][0] = '*';
-    let tempArrayOFeatures = new Array(totalHoles+1).fill('O');
-    tempArrayOFeatures[0] = '^';
-    console.table(array2D)
-    console.table(tempArrayOFeatures)
+    let array2D = new Array(numRows).fill().map(() => new Array(numCols).fill(feature.fieldCharacter));
+    array2D[0][0] = feature.pathCharacter;
+    let tempArrayOFeatures = new Array(totalHoles+1).fill(feature.hole);
+    tempArrayOFeatures[0] = feature.hat;
+    // console.table(array2D)
+    // console.table(tempArrayOFeatures)
     let featuresCount = 0;
-    while(featuresCount < totalHoles+1){
+    while(featuresCount < totalHoles+1){ // distribute each feature (holes 'O' and hat '^') randomly accross the field
         const randRow = Math.floor(Math.random() * numRows);
         const randCol = Math.floor(Math.random() * numCols);
         const randCell = array2D[randRow][randCol];
-        if(randCell === '░'){
+        if(randCell === feature.fieldCharacter){
             array2D[randRow][randCol] = tempArrayOFeatures.pop();
             featuresCount++
         }
@@ -51,17 +90,17 @@ class Field {
     }); 
   }
 
-  updatePath(direction){
+  updatePath(input){
     let newX = this._x;
     let newY = this._y;
-    switch (direction) {
-        case 'up':
+    switch (input) {
+        case direction.up:
             newY--; break;
-        case 'down':
+        case direction.down:
             newY++; break;
-        case 'left':
+        case direction.left:
             newX--; break;
-        case 'right':
+        case direction.right:
             newX++; break; 
     }
     // console.log(`newX: ${newX}, newY: ${newY}`);
@@ -83,16 +122,16 @@ class Field {
     
     this._x = newX;
     this._y = newY;
-    this._field[this._y][this._x] = '*';
+    this._field[this._y][this._x] = feature.pathCharacter;
     return true;
   }
 
   isHole(newX,newY){
-    return this._field[newY][newX] === 'O';
+    return this._field[newY][newX] === feature.hole;
   }
 
   isHat(newX,newY){
-    return this._field[newY][newX] === '^';
+    return this._field[newY][newX] === feature.hat;
   }
 
   isOutOfBounds(newX, newY){
@@ -104,57 +143,10 @@ class Field {
   }
 }
 
-const myField = new Field([
-  ['░', '░', 'O'],
-  ['░', 'O', '░'],
-  ['░', '^', '░'],
-]);
+const fieldEasy = new Field(Field.generateField(5,10,0.2));
+const fieldMedium = new Field(Field.generateField(10,20,0.3));
+const fieldHard = new Field(Field.generateField(25,50,0.4));
 
-// myField.print();
-
-// move('down',myField);
-// move('down',myField);
-// move('right',myField);
-
-
-function move(direction, field){
-    try{
-        field.updatePath(direction);
-        // field.print();
-        return true;
-    } catch(e){
-        console.error(e.message);
-        return false;
-    }
-}
-
-function getDirection(){
-    while(true){    
-        let input = prompt('Which way? ').toLowerCase();
-        switch(input){
-            case 'u':
-                return 'up';
-            case 'd':
-                return 'down';
-            case 'l':
-                return 'left';
-            case 'r':
-                return 'right';
-            default:
-                const error = new Error("Invalid input. Please one of the folowing characters: U (up), D (down), L (left) or R (right)");
-                console.error(error.message);
-        }
-    }
-}
-
-
-const autoField = new Field(Field.generateField(5,10,0.2));
-
-// autoField.print()
-
-let gameOver = false;
-while(!gameOver){
-    autoField.print();
-    let direction = getDirection();
-    gameOver = !move(direction,autoField);
-}
+// fieldEasy.runGame();
+fieldMedium.runGame();
+// fieldHard.runGame();
